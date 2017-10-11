d95b6cf39d151914eeecde77589ed5a6b21b88f7
Josue Martinez <jrb.11@hotmail.com>
2017-10-11 16:58:39 -0400

    Primer Commit

--- src/Principal/principal.java
+++ src/Principal/principal.java
@@ -7,17 +7,43 @@ package Principal;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Rental Servicios
  */
-public class principal implements Initializable{
+public class principal extends Application{
+
+      
+    @Override
+    public void start(Stage stage) throws Exception {
+ Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+ 
+Scene scene = new Scene(root);
+ 
+stage.setScene(scene);
+stage.show();
+ }
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        
+    public void init() throws Exception {
+        super.init(); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    public static void main(String[] args) {
+       launch(args);      
+       
+    }
+    
+ 
+
+   
     
 }
